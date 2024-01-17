e1062df64209f44c2cb3aa1d0d4fdf8f399b7c2a
BoyanAndonov <andonov_boyan@yahoo.com>
2024-01-17 09:48:42 +0200

    Create CountRealNumbersTests.cs

---
+++ CountRealNumbersTests.cs
@@ -0,0 +1,80 @@
+using NUnit.Framework;
+
+using System;
+namespace TestApp.Tests;
+
+public class CountRealNumbersTests
+{
+    // празен вход 
+    [Test]
+    public void Test_Count_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        // Arrange
+        int[] input = Array.Empty<int>();
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+
+    // масив с едно число 
+    [Test]
+    public void Test_Count_WithSingleNumber_ShouldReturnCountString()
+    {
+
+        int[] input = new int[] { 1 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("1 -> 1"));
+    }
+
+
+    // колко пъти се срещат числата {1, 1, 5, 5, 5 ,7};
+    //   ("1 -> 2\n5 -> 3\n7 -> 1"));
+    [Test]
+    public void Test_Count_WithMultipleNumbers_ShouldReturnCountString()
+    {
+        int[] input = new int[] {1, 1, 5, 5, 5 ,7};
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("1 -> 2\n5 -> 3\n7 -> 1"));
+    }
+
+
+    //със отрицателни числа
+    [Test]
+    public void Test_Count_WithNegativeNumbers_ShouldReturnCountString()
+    {
+        int[] input = new int[] { -1, -1, -5, -5, -5, -7 };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("-1 -> 2\n-5 -> 3\n-7 -> 1"));
+    }
+
+
+    // тест с числото " 0 "     {0 ,0 ,0 ,0};
+    //       ("0 -> 4"));
+    [Test]
+    public void Test_Count_WithZero_ShouldReturnCountString()
+    {
+        int[] input = new int[] {0 ,0 ,0 ,0};
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("0 -> 4"));
+    }
+}
